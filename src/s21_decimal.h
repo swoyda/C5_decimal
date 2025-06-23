ce06c0c6ba0befd9289e4e33d391ea1f967c3a6e
swoyda <91897250+swoyda@users.noreply.github.com>
2025-06-23 19:51:54 +0300

    Update s21_decimal.h

--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -44,7 +44,7 @@ int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
 int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
 // Умножение "*"
 int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
-// Деление "/"
+// Деление "/"             -
 int s21_div(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
 // Тестовое умножение
 s21_decimal multiplication(s21_decimal value, int exp);
@@ -55,40 +55,40 @@ int s21_normalization(s21_decimal *value_1, s21_decimal *value_2);
 
 // Меньше <
 int s21_is_less(s21_decimal s21_decimal_one, s21_decimal s21_decimal_two);
-// Меньше или равно <=          +
+// Меньше или равно <=
 int s21_is_less_or_equal(s21_decimal s21_decimal_one,
                          s21_decimal s21_decimal_two);
-// Больше >             +
+// Больше >
 int s21_is_greater(s21_decimal s21_decimal_one, s21_decimal s21_decimal_two);
-// Больше или равно >=          +
+// Больше или равно >=
 int s21_is_greater_or_equal(s21_decimal s21_decimal_one,
                             s21_decimal s21_decimal_two);
-// Равно ==                     +
+// Равно ==
 int s21_is_equal(s21_decimal s21_decimal_one, s21_decimal s21_decimal_two);
-// Не равно !=                    +
+// Не равно !=
 int s21_is_not_equal(s21_decimal s21_decimal_one, s21_decimal s21_decimal_two);
 
 // Преобразователи
 
-// Из int                    +
+// Из int
 int s21_from_int_to_decimal(int src, s21_decimal *dst);
-// Из float                  +
+// Из float
 int s21_from_float_to_decimal(float src, s21_decimal *dst);
-// В int                    +
+// В int
 int s21_from_decimal_to_int(s21_decimal src, int *dst);
-// В float                    +
+// В float
 int s21_from_decimal_to_float(s21_decimal src, float *dst);
 
 // Другие функции
 
 // Округляет указанное Decimal число до ближайшего целого числа в сторону
-// отрицательной бесконечности. сделал через s21_add :)  +
+// отрицательной бесконечности. сделал через s21_add :)
 int s21_floor(s21_decimal value, s21_decimal *result);
 // Округляет Decimal до ближайшего целого числа.
 int s21_round(s21_decimal value, s21_decimal *result);
 // выкидывает остаток
 int s21_truncate(s21_decimal value, s21_decimal *result);
-//Возвращает результат умножения указанного Decimal на -1.                    +
+//Возвращает результат умножения указанного Decimal на -1.
 int s21_negate(s21_decimal value, s21_decimal *result);
 
 // Вспомогательные функции
