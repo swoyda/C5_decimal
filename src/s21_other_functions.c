af4a0b9664599b3267fe6659d35531cd1805b77b
swoyda <91897250+swoyda@users.noreply.github.com>
2025-06-23 19:52:25 +0300

    Deleate comms

--- src/s21_other_functions.c
+++ src/s21_other_functions.c
@@ -41,12 +41,9 @@ int s21_round(s21_decimal value, s21_decimal *result) {
     buf.bits[EXP] &= MINUS;
     s21_decimal little_dec = {{0x00000001, 0x00000000, 0x00000000, 0x00000000}};
     if (s21_get_sign(value)) s21_negate(little_dec, &little_dec);
-    // s21_truncate(buf, &buf);
-    // printf("%d\n", tmp);
     if (buf.bits[0] % 2 == 0 && tmp > 5) {
       s21_add(buf, little_dec, &buf);
     } else if (buf.bits[0] % 2 == 1 && tmp > 4) {
-      // printf("a\n");
       s21_add(buf, little_dec, &buf);
     }
     s21_cop_dec(buf, result);
@@ -95,42 +92,15 @@ void s21_gen_dec(s21_decimal *dst) {
   s21_cop_dec(buf, dst);
 }
 
-// int s21_check_float_lower_29(float src) {
-//   char cop_src[1000000];
-//   int result = 1;
-//   sscanf(cop_src, "%f", &src);
-//   if (strlen(cop_src) > 28) {
-//     result = 0;
-//   }
-//   return result;
-// }
-
-// int s21_float_pointer(float src){
-//   char cop_src[30];
-//   int length = 0;
-//   if (src != (int)src) {
-//     sprintf(cop_src, "%f", src);
-//     printf("in string %s\n", cop_src);
-//     char *dot_pos = strchr(cop_src, '.');
-//     if (dot_pos != NULL) {
-//       length = strlen(cop_src) - (dot_pos - cop_src);
-//     }
-//   }
-//   if (src < 0) length--;
-//   return length;
-// }
-
 int s21_div_10(s21_decimal *result, int move) {
   int last_num = 0;
 
   while (move > 0) {
     unsigned long long uns_buffer = result->bits[2];
-    // printf("move = %d\n", move);
     for (int j = 2; j >= 0; j--) {
       if (j == 0) {
         last_num = uns_buffer % 10;
         result->bits[j] = uns_buffer / 10;
-        // printf("uns = %u\n", uns_buffer);
 
       } else {
         last_num = uns_buffer % 10;
@@ -139,7 +109,6 @@ int s21_div_10(s21_decimal *result, int move) {
       }
     }
     move--;
-    // s21_decimal_to_bits(*result);
   }
   return last_num;
 }
@@ -181,12 +150,10 @@ int s21_is_zero(s21_decimal value) {
 void s21_remove_zeros(s21_decimal *value) {
   int last_n = 0, flag = 1;
   s21_decimal buf;
-  // s21_decimal_to_bits(*value);
 
   while (flag) {
     s21_cop_dec(*value, &buf);
     last_n = s21_div_10(&buf, 1);
-    // printf("%d \n", last_n);
     if (last_n || (buf.bits[3]) == MINUS)
       flag = 0;
     else {
@@ -194,9 +161,7 @@ void s21_remove_zeros(s21_decimal *value) {
       value->bits[3] -= MIN_SCALE;
     }
   }
-  // s21_decimal_to_bits(*value);
   if (s21_is_zero(*value)) {
     value->bits[3] = 0;
   }
-  // if (sign) s21_negate(*value, value);
 }
